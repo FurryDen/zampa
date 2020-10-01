278ad6f59a07e0e02a092c3993646ee0e79cfad5
hersel91 <hersel1991@gmail.com>
2020-10-01 17:25:18 +0200

    Patch 18.0.1

--- core/modules/commands/owner/test.py
+++ core/modules/commands/owner/test.py
@@ -1,16 +1,9 @@
 import core.decorators
-from config import Config
-from core.utility.strings import str_admin_command
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from core.sql.repository.group import GroupRepository
 
 @core.decorators.owner.init
 def init(update, context):
-    bot = context.bot
     chat = update.effective_message.chat_id
-    reply = update.message.reply_to_message
-    if reply is not None:
-        bot.forward_message(Config.STAFF_GROUP,
-                    from_chat_id=chat,
-                    message_id=reply.message_id)
-    else:
-        bot.send_message(chat, text="TEST ERRORE")
+    row = GroupRepository().getById([chat])
+    message = "{}".format(row[0])
+    context.bot.send_message(chat,message)
--- core/sql/__init__.py
+++ core/sql/__init__.py
@@ -1,6 +1,12 @@
-__all__ = ["db_connect",
-           "handler_sql",
-           "commands_sql"
-           ]
+#!/usr/bin/env python
+# coding=utf-8
+
+"""The sql files"""
+__all__ = [
+    "db_connect",
+    "handler_sql",
+    "commands_sql",
+    "repository"
+]
 
 from core.sql import *
